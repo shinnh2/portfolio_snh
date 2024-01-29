ffc401ebcc91fa2ab9f08998d7f490c58ae6a190
shinnh2 <nahyeon.shin9@gmail.com>
2024-01-29 23:46:54 +0900

    [feat/layout] Header 컴포넌트 추가

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+	return (
+		<header>
+			<div className="container">
+				이것은 헤더
+				<div className="logo">로고</div>
+			</div>
+		</header>
+	);
+};
+export default Header;
