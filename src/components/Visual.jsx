5e6d458fb43c4eaf35f324cd3a63db1f3745a2cc
shinnh2 <nahyeon.shin9@gmail.com>
2024-01-29 23:47:30 +0900

    [feat/layout] Visual 컴포넌트 추가

---
+++ src/components/Visual.jsx
@@ -0,0 +1,13 @@
+const Visual = () => {
+	return (
+		<div className="visual">
+			<div className="container">
+				<div className="content">
+					<p>SHIN NA HYEON</p>
+					프론트엔드 개발이 가능한 퍼블리셔, 신나현입니다.
+				</div>
+			</div>
+		</div>
+	);
+};
+export default Visual;
