bc99f541520fc10f1f929f6038b5a28765a585da
shinnh2 <nahyeon.shin9@gmail.com>
2024-01-30 21:05:55 +0900

    [feat/visual] Visual 컴포넌트 생성

--- src/components/Visual.jsx
+++ src/components/Visual.jsx
@@ -3,8 +3,9 @@ const Visual = () => {
 		<div className="visual">
 			<div className="container">
 				<div className="content">
-					<p>SHIN NA HYEON</p>
-					프론트엔드 개발이 가능한 퍼블리셔, 신나현입니다.
+					<p className="title">SHIN NA HYEON</p>
+					<p className="sub_title">프론트엔드 개발이 가능한 퍼블리셔, 신나현입니다.</p>
+					<button className="visual_btn">이력서 보기</button>
 				</div>
 			</div>
 		</div>
