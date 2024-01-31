d7a96bdee117e04a9ba4979a7ed491b1e306e0dc
shinnh2 <nahyeon.shin9@gmail.com>
2024-02-01 03:05:54 +0900

    [feat/work] Work 컴포넌트 생성

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,7 +1,21 @@
+import MainTitle from "./MainTitle";
+import ProjectItem from "./ProjectItem";
+import workData from "../data/workData";
+
 const Work = () => {
 	return (
 		<div className="work">
-			<div className="container">경력 작업물</div>
+			<div className="container main">
+				<MainTitle title={"작업물"} subTitle={"경력 프로젝트"} />
+				<div className="projects_total_info">개수: {workData.length}</div>
+				<ul className="project_list">
+					{workData.map((el, idx) => (
+						<li key={idx}>
+							<ProjectItem data={el} />
+						</li>
+					))}
+				</ul>
+			</div>
 		</div>
 	);
 };
