88f9be98edcab4b24cca2d46b2185196da46e54f
shinnh2 <35826626+shinnh2@users.noreply.github.com>
2024-02-02 01:43:45 +0900

    [feat/footer] Footer 컴포넌트 생성 (#10)
    * [feat/footer] Footer 컴포넌트 스타일 추가
    
    * [feat/footer] Footer 컴포넌트 생성
    
    * [feat/footer] 깃헙 아이콘 SVG 컴포넌트 생성
    
    * [feat/footer] 벨로그 아이콘 SVG 컴포넌트 생성

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,33 @@
+import VelogIcon from "./VelogIcon";
+import GithubIcon from "./GithubIcon";
+
 const Footer = () => {
 	return (
 		<div className="footer">
-			<div className="container">푸터</div>
+			<div className="container main">
+				<p className="footer_comment">
+					사용된 이미지들은 모두 상업적 목적이 아닌 포트폴리오 목적으로
+					사용되었습니다.
+				</p>
+				{/* <hr className="footer_line" /> */}
+				<div className="my_links">
+					<a
+						href="https://velog.io/@shinnh2/posts"
+						target="_blank"
+						className="link velog"
+					>
+						<VelogIcon />
+					</a>
+					<a
+						href="https://github.com/shinnh2"
+						target="_blank"
+						className="link github"
+					>
+						<GithubIcon />
+					</a>
+				</div>
+				<p className="my_email">nahyeon.shin9@gmail.com</p>
+			</div>
 		</div>
 	);
 };
