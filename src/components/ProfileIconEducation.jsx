5ec9ae15c3e6124afe56524d8df54eafe62fdcf5
shinnh2 <35826626+shinnh2@users.noreply.github.com>
2024-02-05 21:42:22 +0900

    [opt/style] 디자인 수정 반영, 스타일 추가 (#11)
    * [opt/style] 디자인 수정사항 반영, 스타일 추가
    
    * [opt/style] Header 스타일을 위한 HTML 구조 변경
    
    * [opt/style] 프로필 아이콘 삽입
    
    * [opt/style] 프로필 경력 아이콘 SVG 추가
    
    * [opt/style] 프로필 자격증 아이콘 SVG 추가
    
    * [opt/style] 프로필 학력 아이콘 SVG 추가
    
    * [opt/style] 비주얼 버튼 이력서 링크 주소 추가

---
+++ src/components/ProfileIconEducation.jsx
@@ -0,0 +1,20 @@
+const ProfileIconEducation = () => {
+	return (
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
+			<mask
+				id="mask0_87_1497"
+				maskUnits="userSpaceOnUse"
+				x="0"
+				y="0"
+				width="48"
+				height="48"
+			>
+				<rect width="48" height="48" fill="#D9D9D9" />
+			</mask>
+			<g mask="url(#mask0_87_1497)">
+				<path d="M24.0001 39.6925L11.0001 32.631V21.7849L4.07715 18.0003L24.0001 7.1543L43.923 18.0003V32.3848H40.9231V19.6618L37.0001 21.7849V32.631L24.0001 39.6925ZM24.0001 25.4003L37.6809 18.0003L24.0001 10.6003L10.3193 18.0003L24.0001 25.4003ZM24.0001 36.2772L34.0001 30.8772V23.3849L24.0001 28.8428L14.0001 23.3849V30.8772L24.0001 36.2772Z" />
+			</g>
+		</svg>
+	);
+};
+export default ProfileIconEducation;
--- src/components/Visual.jsx
+++ src/components/Visual.jsx
@@ -4,8 +4,16 @@ const Visual = () => {
 			<div className="container">
 				<div className="content">
 					<p className="title">SHIN NA HYEON</p>
-					<p className="sub_title">프론트엔드 개발이 가능한 퍼블리셔, 신나현입니다.</p>
-					<button className="visual_btn">이력서 보기</button>
+					<p className="sub_title">
+						프론트엔드 개발이 가능한 퍼블리셔, 신나현입니다.
+					</p>
+					<a
+						href="https://my.surfit.io/w/2143632483"
+						target="_blank"
+						className="visual_btn"
+					>
+						이력서 보기
+					</a>
 				</div>
 			</div>
 		</div>
