169ff4d58110751472d329e8f946440edd040cf3
shinnh2 <nahyeon.shin9@gmail.com>
2024-02-06 00:45:29 +0900

    [feat/headerFunc] 스크롤시 Header 모양 변경되도록 이벤트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect, useRef, forwardRef } from "react";
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
 import "./App.css";
@@ -11,9 +11,21 @@ import Project from "./components/Project";
 import Footer from "./components/Footer";
 
 function App() {
+	const [isHeaderFixed, setIsHeaderFixed] = useState(false);
+	useEffect(() => {
+		const onScrollHander = () => {
+			const scrollPosition = window.scrollY + window.innerHeight;
+			const targetPosition = 120 + window.innerHeight;
+			if (scrollPosition > targetPosition) setIsHeaderFixed(true);
+			else setIsHeaderFixed(false);
+		};
+		window.addEventListener("scroll", onScrollHander);
+		return () => window.removeEventListener("scroll", onScrollHander);
+	}, []);
+
 	return (
 		<div className="wrap">
-			<Header />
+			<Header isHeaderFixed={isHeaderFixed} />
 			<Visual />
 			<Profile />
 			<Skills />
