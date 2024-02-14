64aa536f92e917e34c25b28684faef37308905a9
shinnh2 <35826626+shinnh2@users.noreply.github.com>
2024-02-14 20:52:35 +0900

    [feat/topBtn] 상단 스크롤 버튼 추가 (#23)
    * 240214_1 재배포2
    
    * [feat/topBtn] TopButton 컴포넌트 생성
    
    * [feat/topBtn] TopButton 컴포넌트 적용
    
    * [feat/topBtn] TopButton 스타일 추가
    
    * [feat/topBtn] TopButton 모바일 위치 조정

--- src/components/MainTitle.jsx
+++
@@ -1,9 +0,0 @@
-const MainTitle = ({ subTitle, title }) => {
-	return (
-		<div className="main_title">
-			{subTitle ? <p className="sub_title">{subTitle}</p> : null}
-			<h2 className="title">{title}</h2>
-		</div>
-	);
-};
-export default MainTitle;
