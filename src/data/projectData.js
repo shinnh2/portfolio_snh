a7726435bc480e048cb7541419b16954e1e09653
shinnh2 <nahyeon.shin9@gmail.com>
2024-02-20 19:22:15 +0900

    240220_2 배포

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -8,6 +8,7 @@ const projectData = [
 			"Axios를 활용해 API 연결: 이미지, 동영상 등 렌더링",
 			"게시글, 댓글 좋아요 기능 구현",
 			"글로벌 스타일 작업, 전체 CSS 검수, Figma 작업 및 디자인 90% 이상 담당",
+			"(게스트 ID/PW : gameto33guest / guest12!@)",
 		],
 		period: "2023.01 - 2023.02",
 		engagement: "프론트엔드 33% (프론트엔드 3인중 1인)",
