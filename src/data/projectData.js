4c69787eef7e6dc919a89760808fa214b733a707
shinnh2 <nahyeon.shin9@gmail.com>
2024-08-20 20:10:02 +0900

    [doc/prjData] 프로젝트 데이터 추가

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -16,5 +16,22 @@ const projectData = [
 		github: "https://github.com/codestates-seb/seb41_main_033",
 		projectLink: "http://gameto.kr",
 	},
+	{
+		projectName: "CPAYUSIN",
+		proejctDescription: [
+			"프론트엔드로 참여",
+			"Toast Editor 를 활용해 사용자에게 마크다운 또는 위지윅 방식으로 게시글을 작성하도록 제공",
+			"이미지 전송 방식을 base64에서 S3 url로 변경하여 서버로 전송하도록 수정",
+			"소셜 로그인 구현(google, kakao)",
+			"드래그 앤 드롭, 페이지네이션, 공통 커스텀 컴포넌트(Button, Input 등) 제작",
+			"AWS S3를 사용해 프론트 산출물 정적 배포",
+			"반응형, 디자인 시스템 및 전역스타일 CSS, figma 작업 100%",
+		],
+		period: "2023.08 - 2024.07",
+		engagement: "프론트엔드 100%",
+		projectImg: "cpayusin.jpg",
+		github: "https://github.com/shinnh2/cpayusin",
+		projectLink: "https://www.cpayusin.com",
+	},
 ];
 export default projectData;
